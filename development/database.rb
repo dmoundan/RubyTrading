38020d50f1b5a7ccd820ab3e271db98d5dae238c
Dinos Moundanos <dinos7m@gmail.com>
2020-08-27 15:02:03 -0500

    some updates

--- development/database.rb
+++ development/database.rb
@@ -1,19 +1,35 @@
 #!/usr/bin/env ruby
 
+
 require 'sqlite3'
+#require 'java'
+#require 'dbi'
+#require 'dbd/jdbc'
+#require 'jdbc/sqlite3'
 
 class DataBase
 
     def initialize(location, name)  
         @location=location
         @name=name
-        @db = SQLite3::Database.open(@location+"/"+name)
-
+        databasefile=@location+"/"+name
+        @db = SQLite3::Database.open(databasefile)
+ #       @dbh = DBI.connect(
+ #                           "DBI:jdbc:sqlite:#{databasefile}",  # connection string
+ #                           '',                                 # no username for sqlite3
+ #                           '',                                 # no password for sqlite3
+ #                           'driver' => 'org.sqlite.JDBC')      # need to set the driver
     end
 
-    def execute(sql)
-        @db.execute(sql.gsub("CHARACTER SET=UTF8;", ""))
+    def create_table(sql)
+        sql1=sql.gsub("CREATE TABLE", "CREATE TABLE IF NOT EXISTS")
+        sql2=sql1.gsub("CHARACTER SET=UTF8;", "")
+        @db.execute(sql2)
     end
+
+#    def create_and_populate_table(df, tbl)
+#        df.write_sql(@dbh,tbl)
+#    end
 
     def close()
         @db.close if @db
--- development/main.rb
+++ development/main.rb
@@ -6,14 +6,21 @@ require "./development/yf.rb"
 require "./development/database.rb"
 
 def main()
-    acc=YF.new("AAPL", "2020-08-20", "2020-08-26", "1d")
+    acc=YF.new("AAPL", "2020-08-20", "2020-08-27", "1d")
     df=acc.get_prices_short()
     puts(df.inspect())
     db=DataBase.new("./GeneratedDBs", "historical_data.db")
     sql=df.create_sql("aapl_daily")
     puts(sql)
-    db.execute(sql)
+    db.create_table(sql)
+    #db.create_and_populate_table(df, "aapl_daily")
     db.close()
+
+    df.each(:row) do |row|
+        puts(row["Date"])    
+    end
+
+
 end
 
 main()
